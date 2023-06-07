1fd33bcb790146733548e384ee0c4bfd25ec26bf
Jace Young <onurmark1@gmail.com>
2023-06-07 13:36:06 +0900

    Correct windows resize logic.

--- .config/nvim/lua/onurmark/keymap.lua
+++ .config/nvim/lua/onurmark/keymap.lua
@@ -15,12 +15,12 @@ set.smartindent = true
 set.backspace = '2'
 set.hlsearch = true
 set.incsearch = true
-set.diffopt ='vertical'
+set.diffopt = 'vertical'
 set.wildmenu = true
 set.wildmode = 'list:longest,full'
 set.path:append('./**')
+set.listchars = 'tab:→ ,extends:›,precedes:‹,nbsp:·,trail:·'
 set.list = true
-set.listchars='tab:→ ,extends:›,precedes:‹,nbsp:·,trail:·'
 set.showbreak = '\\'
 set.visualbell = true
 set.hidden = true
@@ -38,7 +38,9 @@ set.colorcolumn = '80'
 set.mouse = ''
 
 -- Clear search with <esc>
-map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", {
+  desc = "Escape and clear hlsearch"
+})
 
 -- move to window
 map('n', '<c-h>', '<c-w>h')
@@ -47,38 +49,126 @@ map('n', '<c-k>', '<c-w>k')
 map('n', '<c-l>', '<c-w>l')
 
 -- window
-map('n', '<Leader>wh', '<c-w>h')
-map('n', '<Leader>wj', '<c-w>j')
-map('n', '<Leader>wk', '<c-w>k')
-map('n', '<Leader>wl', '<c-w>l')
-map('n', '<Leader>wd', '<c-w>q')
-map('n', '<Leader>wv', ':vsplit<CR>')
-map('n', '<Leader>wx', ':split<CR>')
+map('n', '<Leader>wh', '<c-w>h', {
+  desc = "Move cursor to left window"
+})
+map('n', '<Leader>wj', '<c-w>j', {
+  desc = "Move cursor to down window"
+})
+map('n', '<Leader>wk', '<c-w>k', {
+  desc = "Move cursor to up window"
+})
+map('n', '<Leader>wl', '<c-w>l', {
+  desc = "Move cursor to right window"
+})
+map('n', '<Leader>wd', '<c-w>q', {
+  desc = "close window"
+})
+map('n', '<Leader>wv', ':vsplit<CR>', {
+  desc = "Vertical split window"
+})
+map('n', '<Leader>wx', ':split<CR>', {
+  desc = "Horizontal split window"
+})
 
--- resize windows 
-map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+local change_winsize = function(d)
+  local v = vim.api
+
+  -- Lua version of a ternery operator
+  d = d and d or "left"
+
+  local lr = d == "left" or d == "right"
+  -- 5 for left right, 3 for up down
+  local amt = lr and 5 or 3
+
+  local pos = v.nvim_win_get_position(0)
+  local w = v.nvim_win_get_width(0)
+  local h = v.nvim_win_get_height(0)
+
+  if lr then
+    amt = pos[2] == 0 and -amt or amt
+  else
+    amt = pos[1] == 0 and -amt or amt
+  end
+
+  w = (d == "left") and (w + amt) or (w - amt)
+  h = (d == "up") and (h + amt) or (h - amt)
+
+  if lr then
+    v.nvim_win_set_width(0, w)
+  else
+    v.nvim_win_set_height(0, h)
+  end
+end
+
+-- resize windows
+map("n", "<S-Up>", function()
+  change_winsize("up")
+end, {
+  desc = "Increase window height"
+})
+map("n", "<S-Down>", function()
+  change_winsize("down")
+end, {
+  desc = "Decrease window height"
+})
+map("n", "<S-Left>", function()
+  change_winsize("left")
+end, {
+  desc = "Decrease window width"
+})
+map("n", "<S-Right>", function()
+  change_winsize("right")
+end, {
+  desc = "Increase window width"
+})
 
 -- buffer
-map('n', '<Leader>bd', ':bprevious <bar> bdelete #<CR>')
-map('n', '<Leader>bn', ':bnext<CR>')
-map('n', '<Leader>bp', ':bprevious<CR>')
-map('n', '<Leader>b<Tab>', ':buffer #<CR>')
+map('n', '<Leader>bd', ':bprevious <bar> bdelete #<CR>', {
+  desc = "Close buffer",
+  silent = true
+})
+map('n', '<Leader>bn', ':bnext<CR>', {
+  desc = "Open next buffer",
+  silent = true
+})
+map('n', '<Leader>bp', ':bprevious<CR>', {
+  desc = "Open previous buffer",
+  silent = true
+})
+map('n', '<Leader>b<Tab>', ':buffer #<CR>', {
+  desc = "Open recently buffer",
+  silent = true
+})
 
 -- Move Lines
-map("v", "<C-n>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
-map("v", "<C-p>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+map("v", "<C-n>", ":m '>+1<cr>gv=gv", {
+  desc = "Move lines down"
+})
+map("v", "<C-p>", ":m '<-2<cr>gv=gv", {
+  desc = "Move lines up"
+})
 
-map('n', '<Leader><Tab>', ':ClangdSwitchSourceHeader<CR>')
+-- Switch c source / header
+map('n', '<Leader><Tab>', ':ClangdSwitchSourceHeader<CR>', {
+  desc = "Switch source/header",
+  silent = true
+})
 
-map('n', '<Leader>ee', vim.diagnostic.open_float)
-map('n', '<Leader>ep', vim.diagnostic.goto_prev)
-map('n', '<Leader>en', vim.diagnostic.goto_next)
-map('n', '<Leader>eq', vim.diagnostic.setloclist)
+-- Diagnostic
+map('n', '<Leader>ee', vim.diagnostic.open_float, {
+  desc = "Float diagnostic"
+})
+map('n', '<Leader>ep', vim.diagnostic.goto_prev, {
+  desc = "Go to previous diagnostic"
+})
+map('n', '<Leader>en', vim.diagnostic.goto_next, {
+  desc = "Go to next diagnostic"
+})
+map('n', '<Leader>eq', vim.diagnostic.setloclist, {
+  desc = "Open diagnostic location list"
+})
 
 vim.api.nvim_create_user_command('Make', 'wa | make <args> | cwindow 3', { nargs = '*' })
 vim.api.nvim_create_user_command('Mesonc', 'wa | :!meson compile -C <args>', { nargs = '*' })
 vim.api.nvim_create_user_command('Mesont', 'wa | :!meson test -C <args>', { nargs = '*' })
-
